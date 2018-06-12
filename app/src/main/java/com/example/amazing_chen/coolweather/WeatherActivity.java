707be73971e16e26672ee73df5827d4a47f6756b
AmazingC <2712201955@qq.com>
2018-06-12 16:15:30 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/example/amazing_chen/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/amazing_chen/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.amazing_chen.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.amazing_chen.coolweather.gson.Forecast;
 import com.example.amazing_chen.coolweather.gson.Weather;
+import com.example.amazing_chen.coolweather.service.AutoUpdateService;
 import com.example.amazing_chen.coolweather.util.HttpUtil;
 import com.example.amazing_chen.coolweather.util.Utility;
 
@@ -180,6 +182,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic(){
--- app/src/main/java/com/example/amazing_chen/coolweather/service/AutoUpdateService.java
+++ app/src/main/java/com/example/amazing_chen/coolweather/service/AutoUpdateService.java
@@ -1,8 +1,21 @@
 package com.example.amazing_chen.coolweather.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.example.amazing_chen.coolweather.util.HttpUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class AutoUpdateService extends Service {
     public AutoUpdateService() {
@@ -13,5 +26,38 @@ public class AutoUpdateService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        updateBingPic();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8*60*60*1000;
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent,flags,startId);
+
+    }
+
+    private void updateBingPic(){
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOKHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_Pic",bingPic);
+                editor.apply();
+            }
+        });
+    }
+
 
 }
